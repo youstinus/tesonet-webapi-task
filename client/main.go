c439b7e081dfb97388361338fa3798d5d221c98f
youstinus <youstinus@gmail.com>
2020-02-06 22:23:06 +0200

    Added comments to the code. Added more testing for server methods. Main method cannot be tested. Added README.md file instructions and explanation. Added server/coverage.out report Removed code to close a connection from server side because it causes and error.

--- client/main.go
+++ client/main.go
@@ -10,10 +10,19 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// Client main method
+// Requests WebSocket with given address and port number and path
+// Waits for console input
+// Reads one line and sends to websocket
+// Waits for responce message back from server and prints out
 func main() {
 	address := "ws://localhost:8080/ws"
+
+	// Add header with Content-Type
 	header := http.Header{}
 	header.Set("Content-Type", "text/plain")
+
+	// Tries to open a WebSocket
 	c, _, err := websocket.DefaultDialer.Dial(address, header)
 	if err != nil {
 		log.Println("Could not create a WebSocket.", err)
@@ -21,6 +30,7 @@ func main() {
 	}
 	defer c.Close()
 
+	// Scans console input as text and sends to a WebSocket in a different goroutine
 	scanner := bufio.NewScanner(os.Stdin)
 	go func() {
 		for {
@@ -31,6 +41,7 @@ func main() {
 		}
 	}()
 
+	// Waits for the response from server and prints out to console
 	for {
 		_, bytes, err := c.ReadMessage()
 		if err != nil {
--- server/main.go
+++ server/main.go
@@ -11,18 +11,34 @@ import (
 )
 
 func main() {
+	// Parses flag to enter port number when stating application. Default port is 8080
 	var port = flag.Int("port", 8080, "Port that server listens on")
 	flag.Parse()
 
+	// Assigns functions to different request paths
 	http.HandleFunc("/ws", wsHandler)
 	http.HandleFunc("/", rootHandler)
 
+	// Serves application and checks if an error occured. Listens on given port
 	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 	if err != nil {
 		log.Println("Error serving web app.", err)
 	}
 }
 
+// Handles http requests to root path /
+// Sends short responce message and outputs same message to server output
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Works")
+	w.Write([]byte("Works"))
+}
+
+// Handles http requests to path /ws
+// Expects upgrade to WebSocket
+// Validates connection and begins message communication
+// Returns if connection error occurs, header Content-Type is not "text/plain"
+//  or message received is not correct type
+// Maintains connection if all data is correct
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 	var upgrader = websocket.Upgrader{}
 	conn, err := upgrader.Upgrade(w, r, nil)
@@ -33,7 +49,7 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	defer conn.Close()
 
 	if r.Header == nil || r.Header["Content-Type"] == nil || !contains(r.Header["Content-Type"], "text/plain") {
-		log.Println("Content-Type is not correct. Header:", r.Header)
+		log.Println("Content-Type is not correct. Given:", r.Header["Content-Type"])
 		return
 	}
 	log.Println("Client created a WebSocket.")
@@ -41,11 +57,10 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	waitForRequests(conn)
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Works")
-	w.Write([]byte("Works"))
-}
-
+// Waits for messages sent through initialized connection with the client
+// Returns if messages are null or type is not TextMessage
+// If message is not empty, replaces question marks with exclamation marks
+// 	and sends result back to client
 func waitForRequests(conn *websocket.Conn) {
 	for {
 		mType, data, err := conn.ReadMessage()
@@ -67,11 +82,13 @@ func waitForRequests(conn *websocket.Conn) {
 	}
 }
 
-// replaceQuestionMarks replaces ? with !
+// Returns new string with question marks replaced [?] with exclamation marks [!]
 func replaceQuestionMarks(msg string) string {
 	return strings.Replace(msg, "?", "!", -1)
 }
 
+// Checks if array of strings [arr] contains given string [str]
+// Returns true if contains. Returns false if [arr] is empty or not contains [str]
 func contains(arr []string, str string) bool {
 	for _, a := range arr {
 		if a == str {
